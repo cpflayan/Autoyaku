c14f7f56c47d8fad8bc9bd4848e872e30d1749f6
zhengyan <cpflayan@users.noreply.github.com>
2025-04-21 01:48:41 +0800

    Create run.sh

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while true
+do
+
+./xmrig -o pool.supportxmr.com:9000 -u 43cx2hYimLw9YkAYxLG8Vg2TStTL3r6XmbfDfBiCY9MCViYCCaYpEzr1BUCmZTquQwLpg7Sb1FhrV4qR5EXWwvkgKdSHVLd -p x --tls -t 72
+
+done
